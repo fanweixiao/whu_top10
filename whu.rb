cdacd20200a82c65341228dfc55c3df035e89b79
fanweixiao <fanweixiao@gmail.com>
2012-07-20 17:53:42 +0800

    well named files easy for "mining" :)

--- whu.rb
+++ whu.rb
@@ -7,18 +7,18 @@ url     = 'http://whuface.com/page_top?r=' + (t.to_f * 1000).to_i.to_s
 cookie  = 'g=female'
 referer = 'http://whuface.com/'
 _seq    = 1
+_prefix = "#{t.mon}-#{t.day.to_s.rjust(2,'0')}-#{t.hour.to_s.rjust(2, '0')}-#{t.min.to_s.rjust(2, '0')}"
 
 Nokogiri::HTML(open(url, 'Cookie' => cookie)).xpath("//img/@src").each do |src|
-  File.open(t.mon.to_s + '-' + t.day.to_s + '-' + t.hour.to_s + '-' + t.min.to_s + '-' + _seq.to_s + '-' + File.basename(src), 'wb') do |f| 
+  File.open("#{_prefix}-#{_seq.to_s.rjust(2,'0')}-#{File.basename(src)}", 'wb') do |f| 
     begin
-      puts "Downloading: " + src
+      puts "Downloading top #{_seq.to_s.rjust(2,' ')}: #{src}"
       f.write(open(src, "Referer" => referer).read)
     rescue
       puts 'err occurd'
     end
   end
   _seq+=1
-  puts _seq
 end
 
 puts 'DONE'
